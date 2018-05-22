522013786c89cd376547005c485218d76a3951bf
caiya55 <33747685+caiya55@users.noreply.github.com>
2018-05-22 12:30:23 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,15 +1,12 @@
-from keras.layers import Input, concatenate, Activation, Add, UpSampling2D
+from keras.layers import Input, Activation, Add
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import Conv2D, Convolution2D, Conv2DTranspose
-from keras.layers.core import Dropout, Dense, Flatten, Lambda
-from keras.layers.merge import Average
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.core import Dense, Flatten, Lambda
 from keras.layers.normalization import BatchNormalization
-from keras.models import Model, Sequential
-
-import keras.backend as K
+from keras.models import Model
 
 from layer_utils import ReflectionPadding2D, res_block
-from subpixel import SubpixelConv2D
+
 # the paper defined hyper-parameter:chr
 channel_rate = 64
 # Note the image_shape must be multiple of patch_shape
@@ -31,14 +28,14 @@ def generator_model():
     inputs = Input(shape=image_shape)
 
     x = ReflectionPadding2D((3, 3))(inputs)
-    x = Conv2D(filters=ngf, kernel_size=(7,7), padding='valid')(x)
+    x = Conv2D(filters=ngf, kernel_size=(7, 7), padding='valid')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     n_downsampling = 2
     for i in range(n_downsampling):
         mult = 2**i
-        x = Conv2D(filters=ngf*mult*2, kernel_size=(3,3), strides=2, padding='same')(x)
+        x = Conv2D(filters=ngf*mult*2, kernel_size=(3, 3), strides=2, padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
 
@@ -48,67 +45,53 @@ def generator_model():
 
     for i in range(n_downsampling):
         mult = 2**(n_downsampling - i)
-        x = UpSampling2D()(x)
-        x = Conv2D(filters=int(ngf * mult / 2),kernel_size=(3,3),padding='same')(x)       
+        x = Conv2DTranspose(filters=int(ngf * mult / 2), kernel_size=(3, 3), strides=2, padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-    x = Conv2D(filters=output_nc, kernel_size=(9,9), padding='same')(x)
+
+    x = ReflectionPadding2D((3, 3))(x)
+    x = Conv2D(filters=output_nc, kernel_size=(7, 7), padding='valid')(x)
     x = Activation('tanh')(x)
 
     outputs = Add()([x, inputs])
-    #outputs = Lambda(lambda z: K.clip(z, -1, 1))(x)
+    # outputs = Lambda(lambda z: K.clip(z, -1, 1))(x)
     outputs = Lambda(lambda z: z/2)(outputs)
 
     model = Model(inputs=inputs, outputs=outputs, name='Generator')
     return model
+
 
 def discriminator_model():
     """Build discriminator architecture."""
     n_layers, use_sigmoid = 3, False
     inputs = Input(shape=input_shape_discriminator)
 
-    x = Conv2D(filters=ndf, kernel_size=(4,4), strides=2, padding='same')(inputs)
+    x = Conv2D(filters=ndf, kernel_size=(4, 4), strides=2, padding='same')(inputs)
     x = LeakyReLU(0.2)(x)
 
     nf_mult, nf_mult_prev = 1, 1
     for n in range(n_layers):
         nf_mult_prev, nf_mult = nf_mult, min(2**n, 8)
-        x = Conv2D(filters=ndf*nf_mult, kernel_size=(4,4), strides=2, padding='same')(x)
+        x = Conv2D(filters=ndf*nf_mult, kernel_size=(4, 4), strides=2, padding='same')(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(0.2)(x)
 
     nf_mult_prev, nf_mult = nf_mult, min(2**n_layers, 8)
-    x = Conv2D(filters=ndf*nf_mult, kernel_size=(4,4), strides=1, padding='same')(x)
+    x = Conv2D(filters=ndf*nf_mult, kernel_size=(4, 4), strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
 
-    x = Conv2D(filters=1, kernel_size=(4,4), strides=1, padding='same')(x)
+    x = Conv2D(filters=1, kernel_size=(4, 4), strides=1, padding='same')(x)
     if use_sigmoid:
         x = Activation('sigmoid')(x)
 
     x = Flatten()(x)
     x = Dense(1024, activation='tanh')(x)
     x = Dense(1, activation='sigmoid')(x)
-#    x = K.mean(x)
+
     model = Model(inputs=inputs, outputs=x, name='Discriminator')
     return model
 
-def discriminator_model1():
-    model = Sequential()
-    if K.image_data_format() == 'channels_first':
-        model.add(Convolution2D(64, (5, 5), padding='same', input_shape=(1, 28, 28)))
-    else:
-        model.add(Convolution2D(64, (5, 5), padding='same', input_shape=(28, 28, 1)))
-    model.add(LeakyReLU())
-    model.add(Convolution2D(128, (5, 5), kernel_initializer='he_normal', strides=[2, 2]))
-    model.add(LeakyReLU())
-    model.add(Convolution2D(128, (5, 5), kernel_initializer='he_normal', padding='same', strides=[2, 2]))
-    model.add(LeakyReLU())
-    model.add(Flatten())
-    model.add(Dense(1024, kernel_initializer='he_normal'))
-    model.add(LeakyReLU())
-    model.add(Dense(1, kernel_initializer='he_normal'))
-    return model
 
 def generator_containing_discriminator(generator, discriminator):
     inputs = Input(shape=image_shape)

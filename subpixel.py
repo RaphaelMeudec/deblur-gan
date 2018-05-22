8cb2cb5cd187c9c6143210c6f954f35ca1eefc25
caiya55 <33747685+caiya55@users.noreply.github.com>
2018-05-22 12:31:40 +0900

    Add files via upload

---
+++ subpixel.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 21 11:51:34 2018
+
+@author: zhang
+"""
+
+import tensorflow as tf
+from keras.layers import Lambda
+
+# https://github.com/twairball/keras-subpixel-conv
+def SubpixelConv2D(input_shape, scale=4):
+    """
+    Keras layer to do subpixel convolution.
+    NOTE: Tensorflow backend only. Uses tf.depth_to_space
+    Ref:
+        [1] Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network
+            Shi et Al.
+            https://arxiv.org/abs/1609.05158
+    :param input_shape: tensor shape, (batch, height, width, channel)
+    :param scale: upsampling scale. Default=4
+    :return:
+    """
+    # upsample using depth_to_space
+    def subpixel_shape(input_shape):
+        dims = [input_shape[0],
+                input_shape[1] * scale,
+                input_shape[2] * scale,
+                int(input_shape[3] / (scale ** 2))]
+        output_shape = tuple(dims)
+        return output_shape
+
+    def subpixel(x):
+        return tf.depth_to_space(x, scale)
+
+
+    return Lambda(subpixel, output_shape=subpixel_shape, name='subpixel')

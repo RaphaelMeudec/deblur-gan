2a43665b648c817e5e08aaa0f8611a4d09f788a4
Raphael <raphael.meudec@gmail.com>
2018-08-28 09:17:49 +0200

    add gradient penalty loss to list of losses

--- losses.py
+++ losses.py
@@ -1,6 +1,7 @@
 import keras.backend as K
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
+import numpy as np
 
 # Note the image_shape must be multiple of patch_shape
 image_shape = (256, 256, 3)
@@ -23,3 +24,15 @@ def perceptual_loss(y_true, y_pred):
 
 def wasserstein_loss(y_true, y_pred):
     return K.mean(y_true*y_pred)
+
+
+def gradient_penalty_loss(self, y_true, y_pred, averaged_samples):
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    gradients_sqr = K.square(gradients)
+    gradients_sqr_sum = K.sum(gradients_sqr,
+                              axis=np.arange(1, len(gradients_sqr.shape)))
+
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    gradient_penalty = K.square(1 - gradient_l2_norm)
+
+    return K.mean(gradient_penalty)
